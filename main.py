bba84b84b63462ce86a9fd7fca7e223923a70257
T1hq3 <161884113+T1hq3@users.noreply.github.com>
2026-02-17 17:36:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ UPDATE_INTERVAL = 7200  # 2 hours
 
 update_lock = threading.Lock()
 
-@@ -111,162 +112,175 @@ def download_excel(session):
+ def download_excel(session):
         raise Exception(f"Excel download failed: {response.status_code}")
 
     with open(EXCEL_FILE, "wb") as f:
